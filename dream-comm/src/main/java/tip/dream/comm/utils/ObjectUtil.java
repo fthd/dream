888af2039107acb91e17e03ffaf1cb6e3a9a1d62
tangjx <tangjx@koal.com>
2020-08-13 16:48:17 +0800

    初始化项目

---
+++ dream-comm/src/main/java/tip/dream/comm/utils/ObjectUtil.java
@@ -0,0 +1,10 @@
+package tip.dream.comm.utils;
+
+/**
+ * Object对象处理工具类
+ *
+ * @author tangjx
+ * @date 2020/8/13 16:40
+ */
+public class ObjectUtil {
+}
---
+++ dream-comm/src/main/java/tip/dream/comm/utils/StringUtil.java
@@ -0,0 +1,10 @@
+package tip.dream.comm.utils;
+
+/**
+ * 字符串处理工具类
+ *
+ * @author tangjx
+ * @date 2020/8/13 16:40
+ */
+public class StringUtil {
+}
